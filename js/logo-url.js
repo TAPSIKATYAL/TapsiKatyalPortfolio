d7cd0ed495a98021e69dbc3ce9fd12b587515c04
TAPSIKATYAL <tapsi.katyal123.tk@gmail.com>
2024-07-12 21:14:43 +0530

    professional portfolio for software dev role

---
+++ js/logo-url.js
@@ -0,0 +1,10 @@
+
+document.getElementById('html-logo').src = "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg";
+
+document.getElementById('css-logo').src = "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" ;
+
+document.getElementById('js-logo').src = "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png";
+
+document.getElementById('react-logo').src = "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg";
+
+document.getElementById('redux-logo').src ="https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png";
