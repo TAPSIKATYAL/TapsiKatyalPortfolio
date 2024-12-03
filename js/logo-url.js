7ab49a60834fc287a9bb669c8b8e0c3d3002c7fa
TAPSIKATYAL <tapsi.katyal123.tk@gmail.com>
2024-12-03 12:29:21 +0530

    added experience section

--- js/logo-url.js
+++ js/logo-url.js
@@ -8,3 +8,19 @@ document.getElementById('js-logo').src = "https://upload.wikimedia.org/wikipedia
 document.getElementById('react-logo').src = "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg";
 
 document.getElementById('redux-logo').src ="https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png";
+
+
+document.getElementById('git-logo').src ="https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg";
+
+
+document.getElementById('github-logo').src ="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg";
+
+document.getElementById('csharp-logo').src ="https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png";
+
+document.getElementById('MySQL-logo').src ="https://logowik.com/content/uploads/images/mysql8604.logowik.com.webp";
+
+document.getElementById('ts-logo').src ="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg";
+
+document.getElementById('scss-logo').src ="https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg";
+
+document.getElementById('jenkins-logo').src ="https://upload.wikimedia.org/wikipedia/commons/e/e9/Jenkins_logo.svg";
